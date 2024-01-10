372d58cd5d76be953fad820a5bba167905d7aac2
aron <114922035+aronpaco@users.noreply.github.com>
2024-01-10 13:13:55 +0200

    day of week

--- script_csv.py
+++ script_csv.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import datetime
+import calendar
 
 # Replace 'your_file.csv' with the actual file path
 file_path = 'MSFT_2014_2018.csv'
@@ -7,4 +9,24 @@ file_path = 'MSFT_2014_2018.csv'
 df = pd.read_csv(file_path)
 
 # Display the first few rows of the DataFrame
-print(df)
+# print(df)
+
+def check_weekday_or_weekend(date):
+    try:
+        # Convert the input date string to a datetime object
+        given_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+         
+        # Use isoweekday() to get the weekday (Monday is 1 and Sunday is 7)
+        day_of_week = (given_date.weekday() + 1) % 7  # Convert Sunday from 6 to 0
+         
+        
+         
+        # Print the result
+        print(f"The day of the week for {given_date.strftime('%Y-%m-%d')} is {day_of_week}")
+         
+    except ValueError as e:
+        print(f"Error: {e}")
+ 
+# Example usage
+date = '2024-01-11'
+check_weekday_or_weekend(date)
